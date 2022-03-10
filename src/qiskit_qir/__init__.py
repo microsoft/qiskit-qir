dfb8d8e5be7a39354868ee1012cc018359d81631
Guen Prawiroatmodjo <guenp@microsoft.com>
2022-03-10 15:25:58 -0800

    add qiskit-qir package and tests

---
+++ src/qiskit_qir/__init__.py
@@ -0,0 +1,10 @@
+##
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+##
+__author__ = """Microsoft Corporation"""
+__email__ = 'que-contacts@microsoft.com'
+__version__ = '0.1.0'
+
+from qiskit_qir.translate import to_qir, to_qir_bitcode
+from qiskit_qir.visitor import SUPPORTED_INSTRUCTIONS
