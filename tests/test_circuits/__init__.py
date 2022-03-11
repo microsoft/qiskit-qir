46c22fc46a143493809ff247062f531a4434f232
Alan Geller <ageller@microsoft.com>
2022-03-11 14:17:32 -0800

    Added a first set of control flow tests

--- tests/test_circuits/__init__.py
+++ tests/test_circuits/__init__.py
@@ -3,11 +3,15 @@
 # Licensed under the MIT License.
 ##
 from test_circuits.basic_circuits import ghz, teleport, unroll
+from test_circuits.control_flow_circuits import *
 from test_circuits.random import *
 from test_circuits.random import __all__
 
 __all__ = [
     "ghz",
     "teleport",
-    "unroll"
+    "unroll",
+    "while_loop",
+    "for_loop",
+    "if_else"
 ] + __all__
