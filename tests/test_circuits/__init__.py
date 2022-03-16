764a0927950fefb838fca5f7db8a378fb5856948
Alan Geller <ageller@microsoft.com>
2022-03-16 10:24:06 -0700

    Minor tweaks

--- tests/test_circuits/__init__.py
+++ tests/test_circuits/__init__.py
@@ -5,10 +5,14 @@
 from test_circuits.basic_circuits import ghz, teleport, unroll
 from test_circuits.control_flow_circuits import *
 from test_circuits.random import *
-from test_circuits.random import __all__
+from test_circuits.random import __all__ as random_fixtures
 
+# All of the test fixtures
 __all__ = [
     "ghz",
     "teleport",
     "unroll"
-] + __all__ + cf_fixtures
+] + random_fixtures + cf_fixtures
+
+# Tests we expect to fail
+__fail__ = cf_fixtures
