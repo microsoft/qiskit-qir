1e71709c9a440f69b395e6148f8937dc41713f55
Alan Geller <ageller@microsoft.com>
2022-03-18 13:17:15 -0700

    Fixed test name duplication and added debug QASM/QIR file output

--- tests/test_qiskit_qir.py
+++ tests/test_qiskit_qir.py
@@ -2,6 +2,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 ##
+from datetime import datetime
+from pathlib import Path
 import pytest
 import logging
 
@@ -12,6 +14,10 @@ from qiskit_qir.translate import to_qir, to_qir_bitcode
 from test_circuits import __all__ as CIRCUITS
 
 _log = logging.getLogger(__name__)
+_test_output_dir = Path(f"test_output.{datetime.now().strftime('%Y%m%d_%H%M')}")
+if _log.isEnabledFor(logging.DEBUG) and not _test_output_dir.exists():
+    _test_output_dir.mkdir()
+
 
 @pytest.mark.parametrize("circuit_name", CIRCUITS)
 def test_visitor(circuit_name, request):
@@ -25,15 +31,20 @@ def test_visitor(circuit_name, request):
 
 
 @pytest.mark.parametrize("circuit_name", CIRCUITS)
-def test_to_qir(circuit_name, request):
+def test_to_qir_string(circuit_name, request):
     circuit = request.getfixturevalue(circuit_name)
     generated_ir = to_qir(circuit)
     _log.debug(generated_ir)
     assert generated_ir is not None
+    if _log.isEnabledFor(logging.DEBUG):
+        qasm_path = _test_output_dir.joinpath(circuit_name + '.qasm')
+        circuit.qasm(filename=str(qasm_path))
+        qir_path = _test_output_dir.joinpath(circuit_name + '.ll')
+        qir_path.write_text(generated_ir)
 
 
 @pytest.mark.parametrize("circuit_name", CIRCUITS)
-def test_to_qir(circuit_name, request):
+def test_to_qir_bitcode(circuit_name, request):
     circuit = request.getfixturevalue(circuit_name)
     generated_bitcode = to_qir_bitcode(circuit)
     _log.debug(generated_bitcode)
