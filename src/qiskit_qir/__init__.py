1a1cd3226ab71d3846cafdccfde431f72ac5406b
Ian Davis <Ian.F.Davis@microsoft.com>
2022-10-31 17:26:32 +0000

    Revert "Updating version to 0.1.0b13 (#28)"
    This reverts commit a68c2dc2ec8ee5fa41397f816c83ad12b5fe858f.

--- src/qiskit_qir/__init__.py
+++ src/qiskit_qir/__init__.py
@@ -4,7 +4,7 @@
 ##
 __author__ = """Microsoft Corporation"""
 __email__ = 'que-contacts@microsoft.com'
-__version__ = '0.1.0b13'
+__version__ = '0.1.0b12'
 
 from qiskit_qir.translate import to_qir, to_qir_bitcode
 from qiskit_qir.visitor import SUPPORTED_INSTRUCTIONS
